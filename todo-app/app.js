7156a495d551a8bb797bc39f4efd55eb7dd86f0a
Kurt F <67562815+fk-pixel@users.noreply.github.com>
2021-09-11 18:54:07 +0200

    added the files to todo-app.js

---
+++ todo-app/app.js
@@ -0,0 +1,74 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => {
+        addTodo(todo);        
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    addTodo();
+});
+
+function addTodo(todo) {
+    let todoText = input.value;
+    if (todo) {
+        todoText = todo.text;
+    }
+    if (todoText) {
+        const todoEl = document.createElement("li");
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+        todoEl.innerText = todoText;
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed");
+            todoCompleted = new JSFrame();
+            todoCompleted.showToast({
+                html:`<span style="color:white";>This TODO is completed</span>`,
+                align:"top",
+                style : { 
+                    borderRadius : '2px', 
+                    backgroundColor : 'green',
+                },
+                duration:2000
+            });
+            updateLS();
+        });
+        todoEl.addEventListener("contextmenu", (e) => {
+            e.preventDefault();
+            todoEl.remove();
+            todoRemoved = new JSFrame();
+            todoRemoved.showToast({
+                html: `<span style="color:white;">This TODO is deleted</span>`,
+                align: "top",
+                style : { 
+                    borderRadius : '2px', 
+                    backgroundColor : 'red',
+                },
+                duration:2000 
+            });
+            updateLS();
+        });
+        todosUL.appendChild(todoEl);
+        input.value = "";
+        updateLS();
+    }
+}
+
+function updateLS() {
+    const todosEl = document.querySelectorAll("li");
+    const todos = [];
+    todosEl.forEach((todoEl) => {
+        todos.push({
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
+        });
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
